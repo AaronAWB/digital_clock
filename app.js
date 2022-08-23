3c99bad2129d4d93f70434d8c05cf557b15c6ef2
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-08-22 22:40:54 -0700

    revise displayCLock and displayDate functions

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-const now = new Date();
+function displayClock () {
+    const currentTime = new Date();
 
-const hours = now.getHours() % 12 || 12;
-const minutes = now.getMinutes().toString().padStart(2, "0");
-const seconds = now.getSeconds().toString().padStart(2, "0");
-let amOrPm = "AM";
+    const hours = currentTime.getHours() % 12 || 12;
+    const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+    const seconds = currentTime.getSeconds().toString().padStart(2, "0");
+    let amOrPm = "AM";
 
-if (now.getHours() >= 12) {
-    amOrPm = "PM";
-}
+    if (currentTime.getHours() >= 12) {
+        amOrPm = "PM";
+    }
 
-const weekNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-const monthNames = ["January","February","March","April","May","June","July",
-    "August","September","October","November","December"];
-
-const day = weekNames[now.getDay()];
-const date = now.getDate();
-const month = monthNames[now.getMonth()];
-const year = now.getFullYear();
-let ordinal;
-
-if (date == 1 || date == 21 || date == 31) {
-    ordinal = "st";
-} else if (date == 2 || date == 22) {
-    ordinal = "nd";
-} else if (date == 3 || date == 23) {
-    ordinal = "rd";
-} else {
-    ordinal = "th";
-}
-
-function displayTime () {
     document.getElementById("clock").innerHTML = `${hours}:${minutes}:${seconds} ${amOrPm}`;
 }
 
 function displayDate () {
+    const currentDate = new Date();
+
+    const weekNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    const monthNames = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+
+    const day = weekNames[currentDate.getDay()];
+    const date = currentDate.getDate();
+    const month = monthNames[currentDate.getMonth()];
+    const year = currentDate.getFullYear();
+    let ordinal;
+
+    if (date == 1 || date == 21 || date == 31) {
+        ordinal = "st";
+    } else if (date == 2 || date == 22) {
+        ordinal = "nd";
+    } else if (date == 3 || date == 23) {
+        ordinal = "rd";
+    } else {
+        ordinal = "th";
+    }
+
     document.getElementById("date").innerHTML = `${day}, ${month} ${date}${ordinal} ${year}`;
 }
-        
-displayTime(now);
-displayDate(now);
+       
+displayClock();
+displayDate();
 
-setInterval(displayTime, 1000);
+setInterval(displayClock, 1000);
 setInterval(displayDate, 1000);
 
 
